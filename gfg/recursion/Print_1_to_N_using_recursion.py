f4d7c590e1835437d01ee6b91cee7fabf5c6cced
abhineet1910 <abhineet1910mitra@gmail.com>
2026-07-10 12:59:20 +0530

    PRINT 1 TO N USING RECURSION

---
+++ gfg/recursion/Print_1_to_N_using_recursion.py
@@ -0,0 +1,32 @@
+# Print 1 to n using Recursion
+#
+# Given an integer n. Print numbers from 1 to n using recursion.
+#
+# Examples:
+#
+# Input: n = 3
+# Output: [1, 2, 3]
+# Explanation: We have to print numbers from 1 to 3.
+#
+# Input: n = 10
+# Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def printNos(n):
+    if n == 0:
+        print(0, end=" ")
+        # base condition
+        return
+
+    if n < 0:
+        printNos(n + 1)
+        print(n + 1, end=" ")
+        return  # <- important!
+
+    # recursive call first
+    printNos(n - 1)
+
+    # print after recursion
+    print(n, end=' ')
+
+printNos(10)
+printNos(-11)
