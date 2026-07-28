5c71ad41c59c0713389efcef9c5380bfc4e7189e
abhineet mitra <abhineet1910mitra@gmail.com>
2026-07-28 12:17:37 +0530

    Time: 3 ms (90.13%), Space: 13.5 MB (49.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,20 +1,31 @@
 class Solution(object):
     def moveZeroes(self, nums):
+        # brute force 
 
-        temp = []
-        n = len(nums)
+        # temp = []
+        # n = len(nums)
 
-        # Store all non-zero elements
-        for i in range(n):
-            if nums[i] != 0:
-                temp.append(nums[i])
+        # # Store all non-zero elements
+        # for i in range(n):
+        #     if nums[i] != 0:
+        #         temp.append(nums[i])
 
         
-        for i in range(len(temp)):
-            nums[i] = temp[i]
+        # for i in range(len(temp)):
+        #     nums[i] = temp[i]
 
-        for i in range(len(temp), n):
+        # for i in range(len(temp), n):
+        #     nums[i] = 0
+        # optimmal solution 
+        insert = 0 
+        n = len(nums)
+        for i in range (0,n):
+            if nums[i] != 0:
+                nums[insert] = nums[i]
+                insert += 1
+        for i in range(insert,n):
             nums[i] = 0
+
 
         """
         :type nums: List[int]
