95f4f39335d46e3bdf8155c407022624295816f7
abhineet mitra <abhineet1910mitra@gmail.com>
2026-07-30 21:07:00 +0530

    Time: 5 ms (40.81%), Space: 13.9 MB (79.31%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        res = 0
+        for n in nums:
+            res= n^res
+        return res
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
