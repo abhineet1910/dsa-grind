c03e9b4ea6c4ed21b8ad40d8f8bd92ef2562830f
abhineet mitra <abhineet1910mitra@gmail.com>
2026-08-03 23:53:41 +0530

    Time: 125 ms (18.94%), Space: 47.1 MB (40.47%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def rearrangeArray(self, nums):
+        # brute force this take 
+        # this gonna takes tc of O(2N) AND SC OF O(N)
+        pos = []
+        neg = []
+        n = len(nums)
+        for i in range(n):
+            if nums[i]>0:
+                pos.append(nums[i])
+            else:
+                neg.append(nums[i])
+        for i in range(n/2):
+            nums[i*2]=pos[i]
+            nums[i*2+1]=neg[i]
+        return nums
+        # optimal approch 
+        # ans = []
+        # posindex=0
+        # negindex=1
+        # for i in range(n):
+        #     if nums<0:
+        #         ans[negindex]=nums[i]
+        #         negindex+=2
+        #     else:
+        #         ans[posindex]=nums[i]
+        #         posindex+=2
+        # return ans
+
+
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
