c1884ce68e64fbe72146531c89621462e43bacfe
abhineet mitra <abhineet1910mitra@gmail.com>
2026-08-05 12:54:58 +0530

    Time: 17 ms (23.12%), Space: 13.8 MB (20.78%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -2,18 +2,40 @@ class Solution(object):
     def setZeroes(self, matrix):
         n = len(matrix)
         m = len(matrix[0])
-        col = [0] * m
-        row = [0] * n
+        # optimal approch
+        col0=1
         for i in range(n):
             for j in range(m):
                 if matrix[i][j]==0:
-                    row[i]=1
-                    col[j]=1
-
-        for i in range(n):
-            for j in range(m):
-                if row[i] or col[j]:
+                    matrix[i][0]=0
+                    if j != 0:
+                        matrix[0][j]=0
+                    else:
+                        col0=0
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[0][j]==0 or matrix[i][0]==0:
                     matrix[i][j] = 0
+        if matrix[0][0]==0:
+            for j in range(m):
+                matrix[0][j]=0
+        if col0==0:
+            for i in range(n):
+                matrix[i][0]=0
+        
+        # better approch 
+        # col = [0]*m
+        # row = [0]*n
+        # for i in range(n):
+        #     for j in range(m):
+        #         if matrix[i][j]==0:
+        #             row[i]=1
+        #             col[j]=1
+
+        # for i in range(n):
+        #     for j in range(m):
+        #         if row[i] or col[j]:
+        #             matrix[i][j]=0
         # lets dive brute force 
         # def markRow(i):
         # for j in range(m):
