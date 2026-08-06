704ad090177f4ba9f6617cd28766b946c3d66bc6
abhineet mitra <abhineet1910mitra@gmail.com>
2026-08-07 00:43:53 +0530

    Time: 4 ms (80.16%), Space: 13.6 MB (39.31%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,29 @@
 class Solution(object):
     def majorityElement(self, nums):
-        count = {}
-        result = 0
-        maxcount = 0
-        for n in nums:
-            count[n] = 1 + count.get(n,0)
-            if count[n]>maxcount:
-                result = n
-            maxcount = max(count[n],maxcount)
-        return result 
+        # better approch for this 
+        # count = {}
+        # result = 0
+        # maxcount = 0
+        # for n in nums:
+        #     count[n] = 1 + count.get(n,0)
+        #     if count[n]>maxcount:
+        #         result = n
+        #     maxcount = max(count[n],maxcount)
+        candidate = None
+        count = 0
+
+        for num in nums:
+
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
+        
         """
         :type nums: List[int]
         :rtype: int
